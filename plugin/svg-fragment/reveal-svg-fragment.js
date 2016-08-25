9b0ba3a322811e012a2ab9afb9a83fab75fb60a4
Jeff Cody <jcody@redhat.com>
2016-08-24 23:41:42 -0400

    interim

--- plugin/svg-fragment/reveal-svg-fragment.js
+++ plugin/svg-fragment/reveal-svg-fragment.js
@@ -28,7 +28,7 @@ THE SOFTWARE.
     local = proto === "file:",
     d3 = window.d3,
     defaults = {
-      d3: (local ? "http:" : proto) + "//cdn.jsdelivr.net/d3js/latest/d3.min.js",
+        d3: "./plugin/svg-fragment/d3.min.js",
       selector: "title"
     };
 
